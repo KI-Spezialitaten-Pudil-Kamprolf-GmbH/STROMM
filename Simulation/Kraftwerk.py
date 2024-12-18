d8504812655c14ea77094a400f741fa461c778b2
Rune Kamprolf <runekamprolf@gmail.com>
2024-12-18 21:50:20 +0100

    Genauere Steuerung (1%), Maximaler Überschuss bei 6KWB á 800 MW war 24 MW Keine negativen Werte mehr

--- Simulation/Kraftwerk.py
+++ Simulation/Kraftwerk.py
@@ -4,32 +4,112 @@ from typing import override
 
 
 class KraftwerkStatus(Enum):
-    P00 = 0
-    P0005 = 1
-    P0510 = 2
-    P1015 = 3
-    P1520 = 4
-    P2025 = 5
-    P2530 = 6
-    P3035 = 7
-    P3540 = 8
-    P4045 = 9
-    P4550 = 10
-    P5055 = 11
-    P5560 = 12
-    P6065 = 13
-    P6570 = 14
-    P7075 = 15
-    P7580 = 16
-    P8085 = 17
-    P8590 = 18
-    P9095 = 19
-    P100 = 20
+    P_0 = 0
+    P_1 = 1
+    P_2 = 2
+    P_3 = 3
+    P_4 = 4
+    P_5 = 5
+    P_6 = 6
+    P_7 = 7
+    P_8 = 8
+    P_9 = 9
+    P_10 = 10
+    P_11 = 11
+    P_12 = 12
+    P_13 = 13
+    P_14 = 14
+    P_15 = 15
+    P_16 = 16
+    P_17 = 17
+    P_18 = 18
+    P_19 = 19
+    P_20 = 20
+    P_21 = 21
+    P_22 = 22
+    P_23 = 23
+    P_24 = 24
+    P_25 = 25
+    P_26 = 26
+    P_27 = 27
+    P_28 = 28
+    P_29 = 29
+    P_30 = 30
+    P_31 = 31
+    P_32 = 32
+    P_33 = 33
+    P_34 = 34
+    P_35 = 35
+    P_36 = 36
+    P_37 = 37
+    P_38 = 38
+    P_39 = 39
+    P_40 = 40
+    P_41 = 41
+    P_42 = 42
+    P_43 = 43
+    P_44 = 44
+    P_45 = 45
+    P_46 = 46
+    P_47 = 47
+    P_48 = 48
+    P_49 = 49
+    P_50 = 50
+    P_51 = 51
+    P_52 = 52
+    P_53 = 53
+    P_54 = 54
+    P_55 = 55
+    P_56 = 56
+    P_57 = 57
+    P_58 = 58
+    P_59 = 59
+    P_60 = 60
+    P_61 = 61
+    P_62 = 62
+    P_63 = 63
+    P_64 = 64
+    P_65 = 65
+    P_66 = 66
+    P_67 = 67
+    P_68 = 68
+    P_69 = 69
+    P_70 = 70
+    P_71 = 71
+    P_72 = 72
+    P_73 = 73
+    P_74 = 74
+    P_75 = 75
+    P_76 = 76
+    P_77 = 77
+    P_78 = 78
+    P_79 = 79
+    P_80 = 80
+    P_81 = 81
+    P_82 = 82
+    P_83 = 83
+    P_84 = 84
+    P_85 = 85
+    P_86 = 86
+    P_87 = 87
+    P_88 = 88
+    P_89 = 89
+    P_90 = 90
+    P_91 = 91
+    P_92 = 92
+    P_93 = 93
+    P_94 = 94
+    P_95 = 95
+    P_96 = 96
+    P_97 = 97
+    P_98 = 98
+    P_99 = 99
+    P_100 = 100
 
 
 class Kraftwerk:
-    status = KraftwerkStatus.P00
-    status_map = { KraftwerkStatus.P00: 0 }
+    status = KraftwerkStatus.P_0
+    status_map = { KraftwerkStatus.P_0: 0 }
     leistung = 0
     def __init__(self, initStatus, initStatusMap):
         self.status = initStatus
@@ -47,6 +127,6 @@ class Kraftwerk:
 class Windpark(Kraftwerk):
     @override
     def set_status(self, neuer_status):
-        if (neuer_status not in {KraftwerkStatus.P00, KraftwerkStatus.P100}):
-            raise AttributeError("Windpark unterstützt folgende Modi: P00, P100")
+        if (neuer_status not in {KraftwerkStatus.P_0, KraftwerkStatus.P_100}):
+            raise AttributeError("Windpark unterstützt folgende Modi: 0%, 100%")
         self.status = neuer_status
--- Simulation/Stromverbrauch_und_Produktion_Simulation.py
+++ Simulation/Stromverbrauch_und_Produktion_Simulation.py
@@ -1,10 +1,9 @@
 import random
 import time
-
 from Verbraucher import Verbraucher
 from Kraftwerk import Kraftwerk, KraftwerkStatus, Windpark
 from Energiespeicher import Energiespeicher
-from math import ceil
+from math import ceil, floor
 
 # Simulation
 uhrzeit = 0
@@ -28,46 +27,125 @@ wohngebiet1 = Verbraucher(
 
 anzahl_kraftwerke = int(input("Wieviele Kraftwerksblöcke?"))
 blockleistung = int(input("Was ist die Blockleistung?"))
-# Kraftwerke erstellen und in Liste speichern
 kraftwerke = []
 for i in range(anzahl_kraftwerke):
     kraftwerke.append(Kraftwerk(
-        initStatus=KraftwerkStatus.P00,
+        initStatus=KraftwerkStatus.P_0,
         initStatusMap={
-            KraftwerkStatus.P00: 0,
-            KraftwerkStatus.P0005: ((blockleistung/100)*5),
-            KraftwerkStatus.P0510: ((blockleistung/100)*10),
-            KraftwerkStatus.P1015: ((blockleistung/100)*15),
-            KraftwerkStatus.P1520: ((blockleistung/100)*20),
-            KraftwerkStatus.P2025: ((blockleistung/100)*25),
-            KraftwerkStatus.P2530: ((blockleistung/100)*30),
-            KraftwerkStatus.P3035: ((blockleistung/100)*35),
-            KraftwerkStatus.P3540: ((blockleistung/100)*40),
-            KraftwerkStatus.P4045: ((blockleistung/100)*45),
-            KraftwerkStatus.P4550: ((blockleistung/100)*50),
-            KraftwerkStatus.P5055: ((blockleistung/100)*55),
-            KraftwerkStatus.P5560: ((blockleistung/100)*60),
-            KraftwerkStatus.P6065: ((blockleistung/100)*65),
-            KraftwerkStatus.P6570: ((blockleistung/100)*70),
-            KraftwerkStatus.P7075: ((blockleistung/100)*75),
-            KraftwerkStatus.P7580: ((blockleistung/100)*80),
-            KraftwerkStatus.P8085: ((blockleistung/100)*85),
-            KraftwerkStatus.P8590: ((blockleistung/100)*90),
-            KraftwerkStatus.P9095: ((blockleistung/100)*95),
-            KraftwerkStatus.P100: blockleistung
+            KraftwerkStatus.P_0: 0,
+            KraftwerkStatus.P_1: ((blockleistung/100)*1),
+            KraftwerkStatus.P_2: ((blockleistung/100)*2),
+            KraftwerkStatus.P_3: ((blockleistung/100)*3),
+            KraftwerkStatus.P_4: ((blockleistung/100)*4),
+            KraftwerkStatus.P_5: ((blockleistung/100)*5),
+            KraftwerkStatus.P_6: ((blockleistung/100)*6),
+            KraftwerkStatus.P_7: ((blockleistung/100)*7),
+            KraftwerkStatus.P_8: ((blockleistung/100)*8),
+            KraftwerkStatus.P_9: ((blockleistung/100)*9),
+            KraftwerkStatus.P_10: ((blockleistung/100)*10),
+            KraftwerkStatus.P_11: ((blockleistung/100)*11),
+            KraftwerkStatus.P_12: ((blockleistung/100)*12),
+            KraftwerkStatus.P_13: ((blockleistung/100)*13),
+            KraftwerkStatus.P_14: ((blockleistung/100)*14),
+            KraftwerkStatus.P_15: ((blockleistung/100)*15),
+            KraftwerkStatus.P_16: ((blockleistung/100)*16),
+            KraftwerkStatus.P_17: ((blockleistung/100)*17),
+            KraftwerkStatus.P_18: ((blockleistung/100)*18),
+            KraftwerkStatus.P_19: ((blockleistung/100)*19),
+            KraftwerkStatus.P_20: ((blockleistung/100)*20),
+            KraftwerkStatus.P_21: ((blockleistung/100)*21),
+            KraftwerkStatus.P_22: ((blockleistung/100)*22),
+            KraftwerkStatus.P_23: ((blockleistung/100)*23),
+            KraftwerkStatus.P_24: ((blockleistung/100)*24),
+            KraftwerkStatus.P_25: ((blockleistung/100)*25),
+            KraftwerkStatus.P_26: ((blockleistung/100)*26),
+            KraftwerkStatus.P_27: ((blockleistung/100)*27),
+            KraftwerkStatus.P_28: ((blockleistung/100)*28),
+            KraftwerkStatus.P_29: ((blockleistung/100)*29),
+            KraftwerkStatus.P_30: ((blockleistung/100)*30),
+            KraftwerkStatus.P_31: ((blockleistung/100)*31),
+            KraftwerkStatus.P_32: ((blockleistung/100)*32),
+            KraftwerkStatus.P_33: ((blockleistung/100)*33),
+            KraftwerkStatus.P_34: ((blockleistung/100)*34),
+            KraftwerkStatus.P_35: ((blockleistung/100)*35),
+            KraftwerkStatus.P_36: ((blockleistung/100)*36),
+            KraftwerkStatus.P_37: ((blockleistung/100)*37),
+            KraftwerkStatus.P_38: ((blockleistung/100)*38),
+            KraftwerkStatus.P_39: ((blockleistung/100)*39),
+            KraftwerkStatus.P_40: ((blockleistung/100)*40),
+            KraftwerkStatus.P_41: ((blockleistung/100)*41),
+            KraftwerkStatus.P_42: ((blockleistung/100)*42),
+            KraftwerkStatus.P_43: ((blockleistung/100)*43),
+            KraftwerkStatus.P_44: ((blockleistung/100)*44),
+            KraftwerkStatus.P_45: ((blockleistung/100)*45),
+            KraftwerkStatus.P_46: ((blockleistung/100)*46),
+            KraftwerkStatus.P_47: ((blockleistung/100)*47),
+            KraftwerkStatus.P_48: ((blockleistung/100)*48),
+            KraftwerkStatus.P_49: ((blockleistung/100)*49),
+            KraftwerkStatus.P_50: ((blockleistung/100)*50),
+            KraftwerkStatus.P_51: ((blockleistung/100)*51),
+            KraftwerkStatus.P_52: ((blockleistung/100)*52),
+            KraftwerkStatus.P_53: ((blockleistung/100)*53),
+            KraftwerkStatus.P_54: ((blockleistung/100)*54),
+            KraftwerkStatus.P_55: ((blockleistung/100)*55),
+            KraftwerkStatus.P_56: ((blockleistung/100)*56),
+            KraftwerkStatus.P_57: ((blockleistung/100)*57),
+            KraftwerkStatus.P_58: ((blockleistung/100)*58),
+            KraftwerkStatus.P_59: ((blockleistung/100)*59),
+            KraftwerkStatus.P_60: ((blockleistung/100)*60),
+            KraftwerkStatus.P_61: ((blockleistung/100)*61),
+            KraftwerkStatus.P_62: ((blockleistung/100)*62),
+            KraftwerkStatus.P_63: ((blockleistung/100)*63),
+            KraftwerkStatus.P_64: ((blockleistung/100)*64),
+            KraftwerkStatus.P_65: ((blockleistung/100)*65),
+            KraftwerkStatus.P_66: ((blockleistung/100)*66),
+            KraftwerkStatus.P_67: ((blockleistung/100)*67),
+            KraftwerkStatus.P_68: ((blockleistung/100)*68),
+            KraftwerkStatus.P_69: ((blockleistung/100)*69),
+            KraftwerkStatus.P_70: ((blockleistung/100)*70),
+            KraftwerkStatus.P_71: ((blockleistung/100)*71),
+            KraftwerkStatus.P_72: ((blockleistung/100)*72),
+            KraftwerkStatus.P_73: ((blockleistung/100)*73),
+            KraftwerkStatus.P_74: ((blockleistung/100)*74),
+            KraftwerkStatus.P_75: ((blockleistung/100)*75),
+            KraftwerkStatus.P_76: ((blockleistung/100)*76),
+            KraftwerkStatus.P_77: ((blockleistung/100)*77),
+            KraftwerkStatus.P_78: ((blockleistung/100)*78),
+            KraftwerkStatus.P_79: ((blockleistung/100)*79),
+            KraftwerkStatus.P_80: ((blockleistung/100)*80),
+            KraftwerkStatus.P_81: ((blockleistung/100)*81),
+            KraftwerkStatus.P_82: ((blockleistung/100)*82),
+            KraftwerkStatus.P_83: ((blockleistung/100)*83),
+            KraftwerkStatus.P_84: ((blockleistung/100)*84),
+            KraftwerkStatus.P_85: ((blockleistung/100)*85),
+            KraftwerkStatus.P_86: ((blockleistung/100)*86),
+            KraftwerkStatus.P_87: ((blockleistung/100)*87),
+            KraftwerkStatus.P_88: ((blockleistung/100)*88),
+            KraftwerkStatus.P_89: ((blockleistung/100)*89),
+            KraftwerkStatus.P_90: ((blockleistung/100)*90),
+            KraftwerkStatus.P_91: ((blockleistung/100)*91),
+            KraftwerkStatus.P_92: ((blockleistung/100)*92),
+            KraftwerkStatus.P_93: ((blockleistung/100)*93),
+            KraftwerkStatus.P_94: ((blockleistung/100)*94),
+            KraftwerkStatus.P_95: ((blockleistung/100)*95),
+            KraftwerkStatus.P_96: ((blockleistung/100)*96),
+            KraftwerkStatus.P_97: ((blockleistung/100)*97),
+            KraftwerkStatus.P_98: ((blockleistung/100)*98),
+            KraftwerkStatus.P_99: ((blockleistung/100)*99),
+            KraftwerkStatus.P_100: blockleistung
         }
     ))
 
-industrie1 = Verbraucher(
-    grundbedarf=1250
-)
+industrie1 = Verbraucher(grundbedarf=1250)
+
 windpark1 = Windpark(
-    initStatus=KraftwerkStatus.P100,
+    initStatus=KraftwerkStatus.P_100,
     initStatusMap={
-        KraftwerkStatus.P00: 0,
-        KraftwerkStatus.P100: 500
+        KraftwerkStatus.P_0: 0,
+        KraftwerkStatus.P_100: 500
     }
 )
+
 pumpspeicherwerk = Energiespeicher(
     kapazitaet=2000,
     ladeverlust_anteil=0.1,
@@ -80,65 +158,19 @@ while True:
     else:
         uhrzeit += 1
 
-    # Energiebedarf berechnen
     energiebedarf = wohngebiet1.berechne_bedarf(uhrzeit) + industrie1.berechne_bedarf(uhrzeit)
     energiebedarf = energiebedarf - windpark1.get_leistung()
-
-    # Kohlekraftwerke steuern
     benoetigte_leistung = ceil((energiebedarf / blockleistung) * (100 / anzahl_kraftwerke))
 
     for kraftwerk in kraftwerke:
         if benoetigte_leistung >= 100:
             quit()
-        if benoetigte_leistung == 100:
-            kraftwerk.set_status(  KraftwerkStatus.P100)
-        if benoetigte_leistung < 100 and benoetigte_leistung >= 95:
-            kraftwerk.set_status(  KraftwerkStatus.P9095)
-        if benoetigte_leistung < 95 and benoetigte_leistung >= 90:
-            kraftwerk.set_status(  KraftwerkStatus.P9095)
-        if benoetigte_leistung < 90 and benoetigte_leistung >= 85:
-            kraftwerk.set_status(  KraftwerkStatus.P8590)
-        if benoetigte_leistung < 85 and benoetigte_leistung >= 80:
-            kraftwerk.set_status(  KraftwerkStatus.P8085)
-        if benoetigte_leistung < 80 and benoetigte_leistung >= 75:
-            kraftwerk.set_status(  KraftwerkStatus.P7580)
-        if benoetigte_leistung < 75 and benoetigte_leistung >= 70:
-            kraftwerk.set_status(  KraftwerkStatus.P7075)
-        if benoetigte_leistung < 70 and benoetigte_leistung >= 65:
-            kraftwerk.set_status(  KraftwerkStatus.P6570)
-        if benoetigte_leistung < 65 and benoetigte_leistung >= 60:
-            kraftwerk.set_status( KraftwerkStatus.P6065)
-        if benoetigte_leistung < 60 and benoetigte_leistung >= 55:
-            kraftwerk.set_status(  KraftwerkStatus.P5560)
-        if benoetigte_leistung < 55 and benoetigte_leistung > 50:
-            kraftwerk.set_status(  KraftwerkStatus.P5055)
-        if benoetigte_leistung < 50 and benoetigte_leistung >= 45:
-            kraftwerk.set_status(  KraftwerkStatus.P4550)
-        if benoetigte_leistung < 45 and benoetigte_leistung >= 40:
-            kraftwerk.set_status(  KraftwerkStatus.P4045)
-        if benoetigte_leistung < 40 and benoetigte_leistung >= 35:
-            kraftwerk.set_status(  KraftwerkStatus.P3540)
-        if benoetigte_leistung < 35 and benoetigte_leistung >= 30:
-            kraftwerk.set_status(  KraftwerkStatus.P3035)
-        if benoetigte_leistung < 30 and benoetigte_leistung >= 25:
-            kraftwerk.set_status(  KraftwerkStatus.P2530)
-        if benoetigte_leistung < 25 and benoetigte_leistung >= 20:
-            kraftwerk.set_status(  KraftwerkStatus.P2025)
-        if benoetigte_leistung < 20 and benoetigte_leistung >= 15:
-            kraftwerk.set_status(  KraftwerkStatus.P1520)
-        if benoetigte_leistung < 15 and benoetigte_leistung >= 10:
-            kraftwerk.set_status( KraftwerkStatus.P1015)
-        if benoetigte_leistung < 10 and benoetigte_leistung >= 5:
-            kraftwerk.set_status(  KraftwerkStatus.P0510)
-        if benoetigte_leistung < 5 and benoetigte_leistung > 0:
-            kraftwerk.set_status( KraftwerkStatus.P0005)
-        if benoetigte_leistung == 0:
-            kraftwerk.set_status(  KraftwerkStatus.P00)
+        # Optimierte if-Bedingungen:
+        if 0 < benoetigte_leistung <= 100:
+            kraftwerk.set_status(getattr(KraftwerkStatus, f"P_{int(benoetigte_leistung)}"))
 
-    # Überschuss berechnen
     ueberschuss = sum(kraftwerk.get_leistung() for kraftwerk in kraftwerke) - energiebedarf
 
-    # Ausgabe
     print(f"Uhrzeit: {uhrzeit} Uhr")
     print(f"Energiebedarf insgesamt: {energiebedarf} MW")
     for i, kraftwerk in enumerate(kraftwerke):
